61dae24ca6c156697f48c430d6d07475e280010a
NicolasWaller04 <nico04wc@gmail.com>
2025-10-24 15:19:21 -0300

    Se agregaron los movimientos U2, D2, B2, F2, R2, L2, para completar la lista de movimeintops oficiales del 3x3x3

--- Scramble.java
+++ Scramble.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Scramble {
     public static String RandomMezcla(int CantidadMoviminetos) {
-        String[] movimientos = {"R", "R'", "L", "L'", "U", "U'", "D", "D'", "F", "F'", "B", "B'"};
+        String[] movimientos = {"R", "R'", "R2", "L", "L'", "L2", "U", "U'", "U2", "D", "D'", "D2", "F", "F'", "F2", "B", "B'", "B2"};
         Random random = new Random();
         String mezcla = "";
         String ultimo = " ";
