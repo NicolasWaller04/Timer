94356c7d1dba4eb68a774b8bbbc1809c2db58dad
NicolasWaller04 <nico04wc@gmail.com>
2025-10-24 19:44:44 -0300

    .

--- Timer.java
+++ Timer.java
@@ -7,64 +7,63 @@ public class Timer {
         double Tiempos;
         try (Scanner sc = new Scanner(System.in)){
             System.out.println("Vas a armar un 2x2x2 o un 3x3x3 responde (2x2x2 o 3x3x3)");
-            try (Scanner cubo = new Scanner (System.in)){
-                String QueCubo = cubo.nextLine().toLowerCase();
-                if (QueCubo.equals("3x3x3")){
-                    for (int i = 0; i < 9999; i++){
-                        int num = 1;
-                        System.out.println("Mezcla:");
-                        System.out.println(Scramble3x3x3.RandomMezcla3x3x3(20));
-                        System.out.println("Presiona ENTER para iniciar el cronometro");
-                        sc.nextLine();
-                        RelojContador timer = new RelojContador();
-                        timer.iniciar();
-                        System.out.println("Presiona ENTER para detener el cronometro");
-                        sc.nextLine();
-                        timer.detener();
-                        System.out.println(timer.ObtenerTiempo()+" s");
-                        System.out.println("===============");
-                        Tiempos = timer.ObtenerTiempo(); 
-                        ListaTiempos3x3x3.add(i, Tiempos);
-                        if (Tiempos < MejorTiempo){
-                            MejorTiempo = Tiempos;
-                        }
-                        for (double N : ListaTiempos3x3x3){
-                            System.out.println(num+": "+N);
-                            num++;
-                        }  
-                        System.out.println("===============");
-                        System.out.println("Tu mejor tiempo es de:");
-                        System.out.println(MejorTiempo);
-                        System.out.println("===============");
+            String QueCubo = sc.nextLine().toLowerCase();
+            if (QueCubo.equals("3x3x3")){
+                for (int i = 0; i < 9999; i++){
+                    int num = 1;
+                    System.out.println("Mezcla:");
+                    System.out.println(Scramble3x3x3.RandomMezcla3x3x3(20));
+                    System.out.println("Presiona ENTER para iniciar el cronometro");
+                    sc.nextLine();
+                    RelojContador timer = new RelojContador();
+                    timer.iniciar();
+                    System.out.println("Presiona ENTER para detener el cronometro");
+                    sc.nextLine();
+                    timer.detener();
+                    System.out.println(timer.ObtenerTiempo()+" s");
+                    System.out.println("===============");
+                    Tiempos = timer.ObtenerTiempo(); 
+                    ListaTiempos3x3x3.add(i, Tiempos);
+                    if (Tiempos < MejorTiempo){
+                        MejorTiempo = Tiempos;
                     }
-                }else if (QueCubo.equals("2x2x2")){
-                    for (int i = 0; i < 9999; i++){
-                        int num = 1;
-                        System.out.println("Mezcla:");
-                        System.out.println(Scramble2x2x2.RandomMezcla2x2x2(20));
-                        System.out.println("Presiona ENTER para iniciar el cronometro");
-                        sc.nextLine();
-                        RelojContador timer = new RelojContador();
-                        timer.iniciar();
-                        System.out.println("Presiona ENTER para detener el cronometro");
-                        sc.nextLine();
-                        timer.detener();
-                        System.out.println(timer.ObtenerTiempo()+" s");
-                        System.out.println("===============");
-                        Tiempos = timer.ObtenerTiempo(); 
-                        ListaTiempos2x2x2.add(i, Tiempos);
-                        if (Tiempos < MejorTiempo){
-                            MejorTiempo = Tiempos;
-                        }
-                        for (double N : ListaTiempos2x2x2){
-                            System.out.println(num+": "+N);
-                            num++;
-                        }  
-                        System.out.println("===============");
-                        System.out.println("Tu mejor tiempo es de:");
-                        System.out.println(MejorTiempo);
-                        System.out.println("===============");
+                    for (double N : ListaTiempos3x3x3){
+                        System.out.println(num+": "+N);
+                        num++;
+                    }  
+                    System.out.println("===============");
+                    System.out.println("Tu mejor tiempo es de:");
+                    System.out.println(MejorTiempo);
+                    System.out.println("===============");
+                }
+            }else if (QueCubo.equals("2x2x2")){
+                for (int i = 0; i < 9999; i++){
+                    int num = 1;
+                    System.out.println("Mezcla:");
+                    System.out.println(Scramble2x2x2.RandomMezcla2x2x2(20));
+                    System.out.println("Presiona ENTER para iniciar el cronometro");
+                    sc.nextLine();
+                    RelojContador timer = new RelojContador();
+                    timer.iniciar();
+                    System.out.println("Presiona ENTER para detener el cronometro");
+                    sc.nextLine();
+                    timer.detener();
+                    System.out.println(timer.ObtenerTiempo()+" s");
+                    System.out.println("===============");
+                    Tiempos = timer.ObtenerTiempo(); 
+                    ListaTiempos2x2x2.add(i, Tiempos);
+                    if (Tiempos < MejorTiempo){
+                        MejorTiempo = Tiempos;
                     }
+                    for (double N : ListaTiempos2x2x2){
+                        System.out.println(num+": "+N);
+                        num++;
+                    }  
+                    System.out.println("===============");
+                    System.out.println("Tu mejor tiempo es de:");
+                    System.out.println(MejorTiempo);
+                    System.out.println("===============");
+                    
                 } 
             }
         }
