0a6a42a6cf96d47d232548eaad3a552ca61f6019
NicolasWaller04 <nico04wc@gmail.com>
2025-10-25 15:43:09 -0300

    nd

--- Timer.java
+++ Timer.java
@@ -66,7 +66,7 @@ public class Timer {
                     System.out.println("===============");
                     sc.close();
                 } 
-            }
+            } 
         }
     }
 }
